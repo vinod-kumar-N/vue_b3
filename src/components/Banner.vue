04526e16d04d7836e953c55f18954e2bfeb0cc29
vinod-kumar-n <vinodn.9635@gmail.com>
2018-09-13 10:24:55 +0530

    vue + boot 3

---
+++ src/components/Banner.vue
@@ -0,0 +1,37 @@
+
+<template>
+<div class="banner" :style=calHeight>
+    <div class="header-row common-banner-fix" id="header-row" :style=style>
+   <div class="container  container-table">
+   <div class="vertical-center-row">
+      <div class="row  justify-content-end">     
+            <div class="col-lg-6 col-lg-offset-6">
+                <h1 class="common-banner-head">{{banner.data.heading}}</h1>
+                <h3>{{banner.data.body}}</h3>
+            </div>
+         </div>
+      </div>
+   </div>
+</div>
+</div>
+</template>
+
+<script>
+export default {
+    props:{
+        banner:{
+            type: Object,
+            required: true
+        }
+    },
+    computed: {
+      style () {
+        return 'backgroundImage: url( "' + this.banner.url + '")';
+      },
+      calHeight(){
+          return 'height:'+ (window.innerHeight) +'px';
+      }
+    }
+}
+</script>
+
